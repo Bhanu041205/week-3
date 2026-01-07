6a22305211cd929ba29561ddee997aa05ed20b86
Bhanu <thoratibhanu251@gmail.com>
2026-01-07 09:48:55 +0530

    first commit

---
+++ java/src/controller/mod/A.java
@@ -0,0 +1,17 @@
+package src.controller.mod {
+    public class A {
+       public   static Object x="🍎";
+        String name ="Apple";
+        int prize =20;
+        public A(){
+            System.out.println("\n\t constructor called \n");
+            System.out.println("\n\t Object :"+x);
+            System.out.println("\n\t name :"+name);
+            System.out.println("\n\t prize :"+prize);
+
+
+        }
+    }
+
+    
+}
